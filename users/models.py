2f1264c830191c30e7aeb0ab82848be5b7f5faf3
Alexander Svito <alexandervirk@gmail.com>
2018-09-17 19:23:39 +0300

    add requirements

--- users/models.py
+++ users/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
 	email = models.EmailField()
+    balance = models.DecimalField(decimal_places=2)
 	rating = models.DecimalField()
     emp_title = models.StringField()
     annual_income = models.DecimalField(decimal_places=2)
