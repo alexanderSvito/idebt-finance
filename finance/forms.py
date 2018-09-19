432afc819cad6a2b7371b4c416f403a750fa7c3d
Alexander Svito <alexandervirk@gmail.com>
2018-09-19 13:17:38 +0300

    add credit creation

--- finance/forms.py
+++ finance/forms.py
@@ -1,5 +1,5 @@
 from django.forms import ModelForm
-from finance.models import Offer
+from finance.models import Offer, Issue, Match
 
 
 class OfferForm(ModelForm):
@@ -7,3 +7,15 @@ class OfferForm(ModelForm):
         model = Offer
         fields = ['credit_fund', 'min_loan_size', 'max_loan_size', 'credit_percentage',
                   'is_with_capitalization', 'grace_period', 'return_period']
+
+
+class IssueForm(ModelForm):
+    class Meta:
+        model = Issue
+        fields = ['amount', 'max_overpay', 'max_credit_period']
+
+
+class MatchForm(ModelForm):
+    class Meta:
+        model = Match
+        fields = ['match_type', 'from_id', 'to_id']
--- finance/models.py
+++ finance/models.py
@@ -2,6 +2,7 @@ from enum import Enum
 
 from django.db import models
 from idebt.helpers import get_days_from_to_date
+from users.models import User
 
 
 class DeptStatus(Enum):
@@ -23,6 +24,7 @@ class AbstractLoan(models.Model):
 
 
 class Offer(AbstractLoan):
+    creditor = models.ForeignKey(User, related_name='offers', on_delete=models.SET_NULL)
     credit_fund = models.DecimalField(decimal_places=2)
     min_loan_size = models.DecimalField(decimal_places=2)
     max_loan_size = models.DecimalField(decimal_places=2)
@@ -47,6 +49,7 @@ class Offer(AbstractLoan):
 
 
 class Issue(models.Model):
+    borrower = models.ForeignKey(User, related_name='issues', on_delete=models.SET_NULL)
     amount = models.DecimalField(decimal_places=2)
     max_overpay = models.DecimalField(decimal_places=2)
     max_credit_period = models.IntegerField()
@@ -60,6 +63,8 @@ class Issue(models.Model):
 
 
 class Debt(AbstractLoan):
+    creditor = models.ForeignKey(User, related_name='credits', on_delete=models.SET_NULL)
+    borrower = models.ForeignKey(User, related_name='debts', on_delete=models.SET_NULL)
     created_at = models.DateTimeField(auto_add=True)
     loan_size = models.DecimalField(decimal_places=2)
     credit = models.ForeignKey(Offer, related_name="loans", on_delete=models.SET_NULL)
@@ -93,6 +98,27 @@ class Debt(AbstractLoan):
         days_passed = get_days_from_to_date(self.created_at)
         return self._count_size_for_days(days_passed + 30)
 
+    @classmethod
+    def create_from(cls, offer_id, issue_id):
+        offer = Offer.objects.find(pk=offer_id)
+        issue = Issue.objects.find(pk=issue_id)
+        return cls.objects.create(
+            loan_size=issue.amount,
+            credit=offer,
+            credit_percentage=offer.credit_percentage,
+            is_with_capitalization=offer.is_with_capitalization,
+            grace_period=offer.grace_period,
+            return_period=offer.return_period
+        )
+
+    def save(self, force_insert=False, force_update=False, using=None,
+             update_fields=None):
+        transaction = self.loan_size
+        self.creditor.balance -= transaction
+        self.borrower.balance += transaction
+        self.creditor.save()
+        self.borrower.save()
+        super(Debt, self).save()
 
     def to_json(self):
         return {
@@ -105,3 +131,28 @@ class Debt(AbstractLoan):
             'created_at': self.created_at,
             'is_with_capitalization': self.is_with_capitalization
         }
+
+
+class Match(models.Model):
+    OFFER = 'OFF'
+    ISSUE = 'ISS'
+
+    match_type = models.CharField(max_length=3)
+    from_id = models.BigIntegerField()
+    to_id = models.BigIntegerField()
+
+    @classmethod
+    def is_matched(cls, offer_id, issued_id):
+        offer_match_exists = cls.objects.filter(
+            match_type=cls.OFFER,
+            from_id=offer_id,
+            to_id=issued_id
+        ).exists()
+
+        issue_match_exists = cls.objects.filter(
+            match_type=cls.ISSUE,
+            from_id=issued_id,
+            to_id=offer_id
+        ).exists()
+
+        return offer_match_exists and issue_match_exists
