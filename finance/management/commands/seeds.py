8b7a4cc534e0512abe65f589b09095fca99d57fd
Alexander Svito <alexandervirk@gmail.com>
2018-09-25 17:02:08 +0300

    add seeds command

--- finance/management/commands/seeds.py
+++ finance/management/commands/seeds.py
@@ -1,10 +1,12 @@
 import os
 import random
 
+from finance.forms import MatchForm
 from users.models import *
 from finance.models import *
 from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
+from decimal import Decimal as D
 
 
 DATA_DIR = os.path.join(settings.BASE_DIR, 'idebt','data')
@@ -21,7 +23,9 @@ def get_gender():
 
 
 def get_username():
-    return random.choice(USERNAMES).strip()
+    username = random.choice(USERNAMES)
+    USERNAMES.remove(username)
+    return username.strip()
 
 
 def get_name(gender):
@@ -82,9 +86,124 @@ def create_user():
     }
 
 
+def create_offer(user):
+    credit_fund = round(user.balance * D(random.triangular(0, 1, 0.2)))
+    max_loan_size = round(random.triangular(credit_fund / 10, credit_fund, credit_fund / 5))
+    min_loan_size = round(random.triangular(credit_fund / 20, credit_fund / 10, credit_fund / 15))
+    percentage = random.random() * 10
+    is_with_capitalization = random.random() < 0.25
+    return_period = random.randint(7, 62)
+    grace_period = int(return_period * random.random() * 0.5)
+    return {
+        'credit_fund': credit_fund,
+        'min_loan_size': min_loan_size,
+        'max_loan_size': max_loan_size,
+        'credit_percentage': percentage,
+        'is_with_capitalization': is_with_capitalization,
+        'grace_period': grace_period,
+        'return_period': return_period,
+    }
+
+
+def create_issue(user):
+    amount = round(user.balance * D(random.triangular(0.5, 3.0, 1.25)))
+    max_overpay = amount * random.random() * 5
+    min_credit_period = random.randint(3, 31)
+    return {
+        'amount': amount,
+        'max_overpay': max_overpay,
+        'min_credit_period': min_credit_period
+    }
+
+
 class Command(BaseCommand):
     help = 'Populates the database with seed data'
 
+    def add_arguments(self, parser):
+        parser.add_argument('--users', type=int, default=100)
+        parser.add_argument('--seed', type=int, default=random.randint(0, 10000))
+        parser.add_argument('-t', '--truncate', default=True, action='store_false')
+        parser.add_argument('-d', '--debts', default=True, action='store_false')
+
+    def populate_users(self, num):
+        for i in range(num):
+            new_user, created = User.objects.get_or_create(**create_user())
+            if created:
+                self.stdout.write("User {} {} created".format(
+                    new_user.first_name,
+                    new_user.last_name
+                ))
+
+    def populate_offers_and_issues(self):
+        for user in User.objects.all():
+            if user.is_creditor:
+                anchor = 1
+                while random.random() < anchor:
+                    offer = user.offers.create(**create_offer(user))
+                    user.balance -= offer.credit_fund
+                    user.save()
+                    anchor *= 0.7
+
+            anchor = 0.5 if user.is_creditor else 1
+            while random.random() < anchor:
+                user.issues.create(**create_issue(user))
+                anchor *= 0.65
+
+            self.stdout.write("Offers and Issues for {} {} populated".format(
+                user.first_name,
+                user.last_name
+            ))
+
+    def populate_debts(self):
+        for offer in Offer.objects.all():
+            issues = offer.get_issues()
+            self.stdout.write("Inspecting offer {}. Has {} issues available".format(
+                offer.id,
+                len(issues)
+            ))
+            for issue in issues:
+                if random.random() < 0.7:
+                    form = MatchForm({
+                        "match_type": Match.OFFER,
+                        "from_id": offer.id,
+                        "to_id": issue.id
+                    })
+                    match, matched, debt = form.save()
+                    if matched:
+                        self.stdout.write("Debt created")
+
+        for issue in Issue.objects.all():
+            offers = issue.get_offers()
+            self.stdout.write("Inspecting issue {}. Has {} offers available".format(
+                issue.id,
+                len(offers)
+            ))
+            for offer in offers:
+                if random.random() < 0.8:
+                    form = MatchForm({
+                        "match_type": Match.ISSUE,
+                        "from_id": issue.id,
+                        "to_id": offer.id
+                    })
+                    match, matched, debt = form.save()
+                    if matched:
+                        self.stdout.write("Debt created")
+
     def handle(self, *args, **options):
-        for _ in range(10):
-            self.stdout.write(str(create_user()))
+        random.seed(options['seed'])
+        if options['truncate']:
+            User.objects.all().delete()
+            self.stdout.write("All users deleted")
+            Offer.objects.all().delete()
+            self.stdout.write("All offers deleted")
+            Issue.objects.all().delete()
+            self.stdout.write("All issues deleted")
+            Debt.objects.all().delete()
+            self.stdout.write("All debts deleted")
+
+        self.populate_users(options['users'])
+        self.populate_offers_and_issues()
+
+        if options['debts']:
+            self.populate_debts()
+
--- users/models.py
+++ users/models.py
@@ -8,3 +8,18 @@ class User(AbstractUser):
     emp_title = models.CharField(max_length=256)
     annual_income = models.DecimalField(decimal_places=2, max_digits=10)
     is_creditor = models.BooleanField(default=False)
+    is_locked = models.BooleanField(default=False)
+
+    def to_json(self):
+        return {
+            "username": self.username,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "email": self.email,
+            "balance": self.balance,
+            "rating": self.rating,
+            "emp_title": self.emp_title,
+            "annual_income": self.annual_income,
+            "is_creditor": self.is_creditor,
+            "is_locked": self.is_locked
+        }
