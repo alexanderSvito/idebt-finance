8ea54bcda73e12545872c0fb923bf7c84eda1e19
Alexander Svito <alexandervirk@gmail.com>
2018-09-28 15:47:01 +0300

    add auth

--- idebt/urls.py
+++ idebt/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path, include
-
+from rest_framework_jwt.views import obtain_jwt_token
+from users.views import signup
+from idebt import views
 
 urlpatterns = [
     path('api/', include([
             path('v1/',
                  include('finance.urls')
-            )
+                 )
         ])
-    )
+    ),
+    path(r'signup/', signup),
+    path(r'login/', obtain_jwt_token),
 ]
+
+handler404 = views.custom404
+handler500 = views.custom500
--- users/models.py
+++ users/models.py
@@ -3,12 +3,13 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    balance = models.DecimalField(decimal_places=2, max_digits=10)
-    rating = models.DecimalField(decimal_places=2, max_digits=10)
+    balance = models.DecimalField(decimal_places=2, max_digits=10, default=0)
+    rating = models.DecimalField(decimal_places=2, max_digits=10, default=50)
     emp_title = models.CharField(max_length=256)
     annual_income = models.DecimalField(decimal_places=2, max_digits=10)
     is_creditor = models.BooleanField(default=False)
     is_locked = models.BooleanField(default=False)
+    telephone = models.CharField(max_length=32)
 
     def to_json(self):
         return {
