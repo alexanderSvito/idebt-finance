cb1a54f3806fdfb132f4505287c94ae900aa763c
Alexander Svito <alexandervirk@gmail.com>
2018-10-04 16:30:14 +0300

    fix errors

--- finance/serializers.py
+++ finance/serializers.py
@@ -6,10 +6,11 @@ class OfferSerializer(serializers.ModelSerializer):
     class Meta:
         model = Offer
         fields = (
+            'id',
             'credit_fund',
             'min_loan_size',
             'max_loan_size',
-            'percentage',
+            'credit_percentage',
             'is_with_capitalization',
             'grace_period',
             'return_period',
@@ -20,6 +21,7 @@ class IssueSerializer(serializers.ModelSerializer):
     class Meta:
         model = Issue
         fields = (
+            'id',
             'borrower',
             'amount',
             'max_overpay',
@@ -31,12 +33,13 @@ class DebtSerializer(serializers.ModelSerializer):
     class Meta:
         model = Debt
         fields = (
+            'id',
             'status',
             'initial_size',
             'current_size',
             'size_in_week',
             'size_in_month',
-            'percentage',
+            'credit_percentage',
             'created_at',
             'is_with_capitalization'
         )
